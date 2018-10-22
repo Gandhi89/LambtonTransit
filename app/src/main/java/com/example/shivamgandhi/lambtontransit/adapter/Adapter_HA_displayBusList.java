64e2b354a0890e2b9445804cd211ef0efa6127de
shivamGandhi <shivamgandhi95@gmail.com>
2018-10-22 11:57:17 -0400

    HomeActivity => onClick of listview will show alertdialog or will Toast 'yes', based on current time and busTiming.

--- app/src/main/java/com/example/shivamgandhi/lambtontransit/adapter/Adapter_HA_displayBusList.java
+++ app/src/main/java/com/example/shivamgandhi/lambtontransit/adapter/Adapter_HA_displayBusList.java
@@ -1,6 +1,8 @@
 package com.example.shivamgandhi.lambtontransit.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,13 +17,15 @@ import java.util.List;
 public class Adapter_HA_displayBusList extends BaseAdapter {
 
     Context context ;
-    List<String> busName = new ArrayList<>();
-    List<String> busTiming = new ArrayList<>();
+    List<String> busName ;
+    List<String> busTiming ;
+    String currentTime;
 
-    public Adapter_HA_displayBusList(Context context, List<String> busName, List<String> busTiming){
+    public Adapter_HA_displayBusList(Context context, List<String> busName, List<String> busTiming, String currentTime){
         this.context = context;
         this.busName = busName;
         this.busTiming = busTiming;
+        this.currentTime = currentTime;
     }
 
     @Override
@@ -56,6 +60,21 @@ public class Adapter_HA_displayBusList extends BaseAdapter {
         tv1.setText(bus_name);
         tv2.setText(bus_timing);
 
+        //TODO:- write algorithm to calculate difference in timing
+        int busTimingInMinute = (Integer.parseInt(bus_timing.substring(0,2)) * 60) + Integer.parseInt(bus_timing.substring(3,5));
+        int currentTimeInMinute = (Integer.parseInt(currentTime.substring(0,2)) * 60) + Integer.parseInt(currentTime.substring(3,5));
+
+        Log.d("adapter/busTiming",busTimingInMinute+"");
+        Log.d("adapter/currentTime",currentTimeInMinute+"");
+        int difference = busTimingInMinute - currentTimeInMinute;
+        Log.d("adapter/difference",difference+"");
+
+        if(difference > 0 && difference < 121)
+        {
+            Log.d("adapter/","-----------------");
+            Log.d("adapter/busName",bus_name);
+
+        }
 
         return V;
 
--- app/src/main/java/com/example/shivamgandhi/lambtontransit/screens/HomeActivity.java
+++ app/src/main/java/com/example/shivamgandhi/lambtontransit/screens/HomeActivity.java
@@ -1,11 +1,19 @@
 package com.example.shivamgandhi.lambtontransit.screens;
 
+import android.app.AlarmManager;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.shivamgandhi.lambtontransit.R;
@@ -23,7 +31,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+import static java.lang.Thread.interrupted;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
 
     Button toClgBtn,fromClgBtn;
     ListView listView;
@@ -33,6 +43,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     private  String getBus_towards_URL ="http://192.168.0.21/basic/bus_table_towards.php";
     private  String getCurrentTime_URL ="http://worldclockapi.com/api/json/est/now";
     private  String getBus_away_URL ="http://192.168.0.21/basic/bus_table_away.php";
+    Handler handler;
+    private String current_time = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,14 +52,26 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
         initializeAll();
         onClickEvent();
-        getBusData_towards();
+        getCurrentTime();
+        new CountDownTimer(2000, 1000) {
+            @Override
+            public void onTick(long l) {
 
-        toClgBtn.setBackgroundColor(Color.parseColor("#A76253"));
+            }
+
+            @Override
+            public void onFinish() {
+                getBusData_towards();
+                toClgBtn.setBackgroundColor(Color.parseColor("#A76253"));
+            }
+        }.start();
+
     }// end of onCreate()
 
     private void onClickEvent() {
         toClgBtn.setOnClickListener(this);
         fromClgBtn.setOnClickListener(this);
+        listView.setOnItemClickListener(this);
     }
 
     private void getBusData_towards() {
@@ -87,7 +111,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                                 runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
-                                        mAdapter_HA_displayBusList = new Adapter_HA_displayBusList(HomeActivity.this,busName,busTiming);
+                                        mAdapter_HA_displayBusList = new Adapter_HA_displayBusList(HomeActivity.this,busName,busTiming,current_time);
                                         listView.setAdapter(mAdapter_HA_displayBusList);
                                     }
                                 });
@@ -108,6 +132,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         fromClgBtn = findViewById(R.id.homeActivity_fromClg);
         busName = new ArrayList<>();
         busTiming = new ArrayList<>();
+
     }
 
     @Override
@@ -129,6 +154,49 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void getCurrentTime() {
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(getCurrentTime_URL)
+                        .build();
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Request request, IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(Response response) throws IOException {
+                        if (!response.isSuccessful()){
+                            new IOException("homeActivity/Unexpected code: "+response);
+                        }
+                        else {
+                            String result = response.body().string();
+                            try {
+                                JSONObject jsonObject = new JSONObject(result);
+                                String Time = jsonObject.getString("currentDateTime");
+                                final String currentTime = Time.substring(11,16);
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        current_time = currentTime;
+                                        }
+                                });
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+                handler.postDelayed(this,5000);
+            }
+        },2000);
+
+
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -156,6 +224,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                                 runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
+                                        current_time = currentTime;
                                         Toast.makeText(HomeActivity.this, currentTime, Toast.LENGTH_SHORT).show();
                                     }
                                 });
@@ -206,7 +275,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                                 runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
-                                        mAdapter_HA_displayBusList = new Adapter_HA_displayBusList(HomeActivity.this,busName,busTiming);
+                                        mAdapter_HA_displayBusList = new Adapter_HA_displayBusList(HomeActivity.this,busName,busTiming,current_time);
                                         listView.setAdapter(mAdapter_HA_displayBusList);
                                     }
                                 });
@@ -219,5 +288,44 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
             }
         }).start();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        TextView bTimeTv = view.findViewById(R.id.adapter_bustiming);
+        String bTime = bTimeTv.getText().toString();
+
+        int busTimingInMinute = (Integer.parseInt(bTime.substring(0,2)) * 60) + Integer.parseInt(bTime.substring(3,5));
+        int currentTimeInMinute = (Integer.parseInt(current_time.substring(0,2)) * 60) + Integer.parseInt(current_time.substring(3,5));
+
+        Log.d("adapter/busTiming",busTimingInMinute+"");
+        Log.d("adapter/currentTime",currentTimeInMinute+"");
+        int difference = busTimingInMinute - currentTimeInMinute;
+        Log.d("adapter/difference",difference+"");
+
+        if(difference > 0 && difference < 121)
+        {
+            Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show();
+
+        }
+        else
+        {
+            final AlertDialog.Builder builder1 = new AlertDialog.Builder(HomeActivity.this);
+            builder1.setMessage("You can only book your seat two hours before bus timing.");
+            builder1.setTitle("Error");
+            builder1.setCancelable(true);
+
+            builder1.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+
+            AlertDialog alertDialog = builder1.create();
+            alertDialog.show();
+
+        }
+
     }
 }
