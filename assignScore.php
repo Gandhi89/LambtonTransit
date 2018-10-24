c7b78cbd32d3aac392fdfa97bfa0fe6d548a0030
Shivam Gandhi <shivamgandhi95@gmail.com>
2018-10-24 04:22:57 -0400

    Update assignScore.php

--- assignScore.php
+++ assignScore.php
@@ -3,22 +3,7 @@
 
        require "rb-mysql.php";
       $x   = urldecode($_POST['user_info']);
-       //$y   = urldecode($_POST['score']);
-       //$z  = urldecode($_POST['security_number']);
-
-
-
-
-
-      // if data is correct, THEN connect to db and do stuff
-
-    //  require_once "rb-mysql.php";
-
-
-
-      // if data is correct, THEN connect to db and do stuff
-
-
+ 
       R::setup("mysql:host=localhost;dbname=BusService","root", "");
 
       //1.0 add rows to database[insert into]
@@ -30,7 +15,7 @@
 
       R::store($w);
 
- echo "SUCCESS";
+       echo "SUCCESS";
 
 
  ?>
