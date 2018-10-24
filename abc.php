73c06c29e3a29ff3d567fbc1939949f4aa66c0fb
Shivam Gandhi <shivamgandhi95@gmail.com>
2018-10-24 04:22:16 -0400

    Update abc.php

--- abc.php
+++ abc.php
@@ -2,14 +2,7 @@
 <?php
 
        require "rb-mysql.php";
-       //if($_SERVER['CONTENT_TYPE'] === 'application/json; charset=UTF-8') {
-       //student_id=WHY&password=SO&security_number=111
-         //echo "SERVER CONTENT_TYPE : " . $_SERVER['CONTENT_TYPE'];
-         //header('Content-Type: application/json');
-         //echo "SERVER CONTENT_TYPE : " . $_SERVER['CONTENT_TYPE'];
-         //echo "--";
-         //print_r($_SERVER);
-         //echo "--";
+       
          if(!empty($_POST['student_id']) && !empty($_POST['password']) && !empty($_POST['security_number']))
          {
            print_r($_POST);
@@ -38,22 +31,5 @@
            print_r($_POST);
            echo "Fail";
          }
-
-
-       //}
-
-
-
-
-
-
-      // if data is correct, THEN connect to db and do stuff
-
-      //  require_once "rb-mysql.php";
-
-
-
-
-
 
  ?>
